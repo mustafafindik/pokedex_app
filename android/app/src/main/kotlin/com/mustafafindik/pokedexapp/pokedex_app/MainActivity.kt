121d326df0c18bfd990a64ba31a23fef9bef409a
mustafafindik <mustafa.findik3@gmail.com>
2021-10-17 00:03:04 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/mustafafindik/pokedexapp/pokedex_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mustafafindik.pokedexapp.pokedex_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const kTextColor = Color(0xFF131313);
+const kLightTextColor = Color(0xFFA0A9B3);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
