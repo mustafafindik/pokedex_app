9c06403aa5c7c74f90e6f9a0a6d564d7d80d31c0
mustafafindik <mustafa.findik3@gmail.com>
2021-10-21 01:35:33 +0300

    evolution chain

---
+++ lib/models/evolution_chain.dart
@@ -0,0 +1,6 @@
+class EvolutionChain {
+  late String to;
+  late String from;
+
+  EvolutionChain(this.to, this.from);
+}
--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:pokedex_app/models/evolution_chain.dart';
 import 'package:pokedex_app/models/stat.dart';
 
 class Pokemon{
@@ -14,6 +15,7 @@ class Pokemon{
   late String height;
   late String weight;
   late List<String> weakness;
+  late List<EvolutionChain> evolutionChain;
 
-  Pokemon(this.id, this.name, this.image, this.types, this.color,this.species,this.about,this.height,this.weight,this.stat,this.weakness);
+  Pokemon(this.id, this.name, this.image, this.types, this.color,this.species,this.about,this.height,this.weight,this.stat,this.weakness,this.evolutionChain);
 }
